cf36589dbfecbadec6f5e58f958eedb9fd9e15d0
Faisal Patwary <faisal.patwary@baruchmail.cuny.edu>
2025-05-12 06:52:12 +0000

    security dimension update

---
+++ otc_market/DIM_SECURITY.sql
@@ -0,0 +1,28 @@
+use warehouse compute_wh;
+
+with
+    sec_cte as (
+        select
+            secid as security_id,
+            compid as company_id,
+            symbol as trading_symbol,
+            cusip as cusip_number,
+            issue as security_name_issue,
+            sectype as security_type,
+            class as security_class,
+            securitystatus as security_status
+        from public.otc_data_raw
+    ),
+
+    finra_cte as (
+        select
+            issuesymbolidentifier as trading_symbol,
+            securitydescription as security_description,
+            issuername as company_name,
+            country as country
+        from public.otc_securities_raw
+    )
+
+select s.*, f.security_description, f.company_name, f.country
+from sec_cte s
+left join finra_cte f on s.trading_symbol = f.trading_symbol
