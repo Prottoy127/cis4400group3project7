f304660bc54ec177884d2a89b8573ff4f074581f
Faisal Patwary <faisal.patwary@baruchmail.cuny.edu>
2025-05-21 00:33:54 +0000

    dim_source update

--- models/otc_market/DIM_SOURCE.sql
+++ models/otc_market/DIM_SOURCE.sql
@@ -2,7 +2,7 @@
 
 with
     source_cte as (
-        select distinct upper(issuername) as source_name from public.otc_securities_raw
+        select distinct upper(issuername) as source_name, issuesymbolidentifier as trading_symbol from public.otc_securities_raw
     )
 
 select row_number() over (order by source_name) as source_id, source_name
